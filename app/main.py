8a78cad0a3bbfd3180ad15351cba6dfe6b434ea4
Mike Jerrson Galindez <mike.galindez@pds.com.ph>
2024-02-20 14:18:33 +0800

    DELETE & UPDATE endpoint now working

--- app/main.py
+++ app/main.py
@@ -30,27 +30,53 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    cursor.execute("""SELECT * FROM posts;""")
+    cursor.execute("""SELECT * FROM posts""")
     posts = cursor.fetchall()
 
     return {"data": posts}
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_post(post: PostSchema):
-    cursor.execute("""INSERT INTO posts (title, content, is_published) VALUES (%s, %s, %s) RETURNING *;""", (post.title, post.content, post.is_published))
-    new_post = cursor.fetchone()
-    connection.commit()
-    
-    return {"data": new_post}
-
-
 @app.get("/posts/{post_id}")
 async def get_post(post_id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s;""", (str(post_id)))
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(post_id)))
     post = cursor.fetchone()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
 
     return {"data": post}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostSchema):
+    cursor.execute("""INSERT INTO posts (title, content, is_published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.is_published))
+    new_post = cursor.fetchone()
+    connection.commit()
+    
+    return {"data": new_post}
+
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(post_id: int):
+    cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""", (str(post_id)))
+    deleted_post = cursor.fetchone()
+
+    if deleted_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
+
+    connection.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: int, post:PostSchema):
+    cursor.execute("""UPDATE posts SET title=%s, content=%s, is_published=%s WHERE id=%s RETURNING *""", (post.title, post.content, post.is_published, str(post_id)))
+    updated_post = cursor.fetchone()
+
+    if updated_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
+
+    connection.commit()
+
+    return {"data": updated_post}
