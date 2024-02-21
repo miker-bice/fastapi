c1ece115969adfe95f23410541db8354de838d99
Mike Jerrson Galindez <mike.galindez@pds.com.ph>
2024-02-21 15:39:37 +0800

    User model created

--- app/models.py
+++ app/models.py
@@ -12,3 +12,12 @@ class Post(Base):
     content = Column(String, nullable=False)
     is_published = Column(Boolean, server_default="TRUE", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, nullable=False, unique=True, autoincrement=True, primary_key=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    
