6c4443e447c09ab4b688ed1d18243afe2fac076f
Mike Jerrson Galindez <mike.galindez@pds.com.ph>
2024-02-26 18:09:19 +0800

    refactor

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,15 +11,15 @@ router = APIRouter(
 
 
 @router.get("", response_model=List[schemas.PostResponse])
-async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+async def get_posts(db: Session = Depends(get_db), current_user: dict = Depends(oauth2.get_current_user)):
+    posts = db.query(models.Post).all()
 
     return posts
 
 
 @router.get("/{post_id}", response_model=schemas.PostResponse)
-async def get_post(post_id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == post_id, models.Post.owner_id == current_user.id).first()
+async def get_post(post_id: int, db: Session = Depends(get_db), current_user: dict = Depends(oauth2.get_current_user)):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
@@ -28,7 +28,7 @@ async def get_post(post_id: int, db: Session = Depends(get_db), current_user: in
 
 
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
-async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: dict = Depends(oauth2.get_current_user)):
     new_post = models.Post(owner_id = current_user.id, **post.model_dump())
     db.add(new_post)
     db.commit()
@@ -38,7 +38,7 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), c
 
 
 @router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(post_id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+async def delete_post(post_id: int, db: Session = Depends(get_db), current_user: dict = Depends(oauth2.get_current_user)):
     deleted_post = db.query(models.Post).filter(models.Post.id == post_id)
     post = deleted_post.first()
 
@@ -56,7 +56,7 @@ async def delete_post(post_id: int, db: Session = Depends(get_db), current_user:
 
 
 @router.put("/{post_id}", response_model=schemas.PostResponse)
-def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: dict = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == post_id)
     updated_post = post_query.first()
 
